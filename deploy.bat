7114d1e5b2232c59cf261f3d41f82f9359f32ed5
ib-m-j <ib.m.jorgensen@gmail.com>
2016-01-30 00:12:18 +0100

    now whole sequence can be carried out

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,5 @@ copy draganddrop.html ..\..\einarftp\pagaten
 copy registerplay.html ..\..\einarftp\pagaten
 copy testscroll.html ..\..\einarftp\pagaten
 copy nytbord.html  ..\..\einarftp\pagaten
+copy confirm.html  ..\..\einarftp\pagaten
 python ..\pagaten\deploy_pagaten.py	
