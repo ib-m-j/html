5bc3952042c33aa41633dc5e17739e3084e545f7
ib-m-j <ib.m.jorgensen@gmail.com>
2016-02-03 23:24:48 +0100

    started atools file with core javascript functions - to be included all over

--- deploy.bat
+++ deploy.bat
@@ -2,5 +2,7 @@ copy draganddrop.html ..\..\einarftp\pagaten
 copy registerplay.html ..\..\einarftp\pagaten
 copy testscroll.html ..\..\einarftp\pagaten
 copy nytbord.html  ..\..\einarftp\pagaten
+copy environment.html  ..\..\einarftp\pagaten
+copy tools.js ..\..\einarftp\pagaten
 copy confirm.html  ..\..\einarftp\pagaten
 python ..\pagaten\deploy_pagaten.py	
---
+++ tools.js
@@ -0,0 +1,20 @@
+function getUrlVars() {
+		var vars = {};
+		var parts = location.search.replace(
+						/[?&]+([^=&]+)=([^&]*)/gi, 
+				function(m,key,value) {vars[key] = value;});
+return vars;
+}
+
+function getOrderedUrlVars() {
+var vars = [];
+var parts = location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, 
+               function(m,key,value) {vars.push([key, value])});
+return vars;
+}
+
+function setTextsOfRibbonElement(el, text) {
+ //assuming text[0] is header and text[ 1] is content
+    $(el).find('span').text(decodeURI(text[0]));
+    $(el).find('.ribbonElementContent').html(decodeURI(text[1]));
+}
